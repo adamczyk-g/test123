d7ed9142a366fdfea4d6b91df8d17bdfe548d9dc
Grzegorz Adamczyk <46348032+adamczyk-g@users.noreply.github.com>
2024-11-03 21:57:23 +0100

    Add files via upload

--- crypt.sh
+++ crypt.sh
@@ -34,6 +34,10 @@ echo "Dostosowywanie systemu plików..."
 e2fsck -f /dev/mapper/$DISK_CRYPT
 resize2fs /dev/mapper/$DISK_CRYPT
 
+# Montowanie zaszyfrowanej partycji
+echo "Montowanie zaszyfrowanej partycji..."
+mount /dev/mapper/$DISK_CRYPT /mnt/dev
+
 # Montowanie katalogów w chroot
 echo "Montowanie niezbędnych katalogów..."
 mount --bind /dev /mnt/dev
@@ -41,17 +45,13 @@ mount --bind /dev/pts /mnt/dev/pts
 mount --bind /sys /mnt/sys
 mount --bind /proc /mnt/proc
 
-# Montowanie zaszyfrowanej partycji
-echo "Montowanie zaszyfrowanej partycji..."
-mount /dev/mapper/$DISK_CRYPT /mnt
-
 # Wejście do chroot
 echo "Wejście do środowiska chroot..."
 chroot /mnt
 
 # Modyfikacja /etc/crypttab
 UUID=$(blkid -s UUID -o value $DISK)
-echo "$DISK_CRYPT /dev/disk/by-uuid/$UUID none luks" >> /etc/crypttab
+echo "$DISK_CRYPT UUID=$UUID none luks" >> /etc/crypttab
 
 # Modyfikacja /etc/fstab
 echo "Zakomentowanie istniejącej linii dotyczącej partycji root w /etc/fstab..."
@@ -61,7 +61,7 @@ sed -i.bak "/$(echo $DISK | sed 's/\/dev\///')/s/^/#/" /etc/fstab
 echo "/dev/mapper/$DISK_CRYPT / ext4 defaults 0 1" >> /etc/fstab
 
 # Modyfikacja /etc/default/grub
-sed -i "s/^GRUB_CMDLINE_LINUX_DEFAULT=\"/GRUB_CMDLINE_LINUX_DEFAULT=\"cryptdevice=\/dev\/$DISK:$DISK_CRYPT /" /etc/default/grub
+echo "GRUB_ENABLE_CRYPTODISK=y" >> /etc/default/grub
 
 # Zainstalowanie GRUB na odpowiednim dysku
 echo "Instalowanie GRUB na $DISK..."
