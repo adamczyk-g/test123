02b8b8ad876b129b0245e24113717e63e07d6f27
Grzegorz Adamczyk <46348032+adamczyk-g@users.noreply.github.com>
2024-11-21 08:08:06 +0100

    Add files via upload

---
+++ initram.sh
@@ -0,0 +1,23 @@
+DISK="/dev/sda2"
+
+mkdir -p /etc/restricted
+chmod 0700 /etc/restricted
+dd if=/dev/urandom of=/etc/restricted/luks.key bs=4096 count=1
+cryptsetup luksAddKey $DISK /etc/restricted/luks.key
+chown root:root /etc/restricted/luks.key
+chmod 400 /etc/restricted/luks.key
+
+cat << 'EOF' > /etc/initramfs-tools/hooks/xyz-crypttab
+
+#!/bin/sh
+. /usr/share/initramfs-tools/hook-functions
+copy_exec /etc/restricted/luks.key /cryptroot/luks.key
+cp /etc/crypttab "${DESTDIR}/cryptroot/crypttab"
+exit 0
+EOF
+
+chmod +x /etc/initramfs-tools/hooks/xyz-crypttab
+
+update-initramfs -u
+
+echo "Pamiętaj, aby zrestartować system."
