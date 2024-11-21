da1b214648e5f8d7a744ebe9a4cc056b9ec073e3
Grzegorz Adamczyk <46348032+adamczyk-g@users.noreply.github.com>
2024-11-21 12:46:42 +0100

    Add files via upload

--- initram.sh
+++ initram.sh
@@ -1,22 +1,24 @@
 DISK="/dev/sda2"
 
-mkdir -p /etc/restricted
-chmod 0700 /etc/restricted
-dd if=/dev/urandom of=/etc/restricted/luks.key bs=4096 count=1
-cryptsetup luksAddKey $DISK /etc/restricted/luks.key
-chown root:root /etc/restricted/luks.key
-chmod 400 /etc/restricted/luks.key
+mkdir -p /cryptroot
+chmod 0700 /cryptroot
+dd if=/dev/urandom of=/cryptroot/luks.key bs=4096 count=1
+cryptsetup luksAddKey $DISK /cryptroot/luks.key
+chown root:root /cryptroot/luks.key
+chmod 400 /cryptroot/luks.key
 
-cat << 'EOF' > /etc/initramfs-tools/hooks/pre-crypttab
+cat << 'EOF' > /etc/initramfs-tools/hooks/cp-crypttab-and-key
 #!/bin/sh
 . /usr/share/initramfs-tools/hook-functions
-copy_exec /etc/restricted/luks.key /cryptroot/luks.key
+copy_exec /cryptroot/luks.key /cryptroot/luks.key
 copy_exec /etc/crypttab "${DESTDIR}/cryptroot/crypttab"
 exit 0
 EOF
 
-chmod +x /etc/initramfs-tools/hooks/pre-crypttab
+chmod +x /etc/initramfs-tools/hooks/cp-crypttab-and-key
 
 update-initramfs -u
 
 echo "Pamiętaj, aby zrestartować system."
+
+#cryptsetup luksOpen /dev/sda2 root-crypt
